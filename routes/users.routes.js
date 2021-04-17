349bab169c9b11112917e03c6a00cfee91b8c0eb
Netta Avidan <nettavidan19@gmail.com>
2021-04-17 23:58:06 +0300

    Initial commit

---
+++ routes/users.routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/users.controller')
+
+router.get('/', (req,res) =>{
+    //return res.status(200).json({users : users})
+    userController.getAllUsers(req,res)
+}).post('/', (req,res) =>{
+    userController.addUser(req,res)
+}).get('/:Id',(req,res) =>{
+    userController.getUserById(req,res)
+}).put('/:id/deposit', (req,res) =>{
+    userController.deposit(req,res)
+}).put('/:id/credit', (req,res) =>{
+    userController.updateCredit(req,res)
+}).put('/:id/withdraw', (req,res) =>{
+    userController.withdrawMoney(req,res)
+}).put('/transfer',(req,res)=>{
+    userController.transferMoney(req,res)
+})
+
+module.exports = router;
