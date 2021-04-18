432c899b689659820fb79ca5fe7f97f86c1a6dfb
Netta Avidan <nettavidan19@gmail.com>
2021-04-18 15:35:30 +0300

    react

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ const usersRoute = require('./routes/users.routes');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const db=fs.readFileSync('../data/users.json').toString()
+dbb=JSON.parse(db)
+
 app.use('/bank/users',usersRoute);
 
-app.listen(port,()=>{
-    console.log(`bank's app start at ${port}`)
-})
+// app.listen(port,()=>{
+//     console.log(`bank's app start at ${port}`)
+// })
+
+app.get('/',(req,res)=>{
+    //return res.json({success : {id:12,email : 'nully@gmail.com'}})
+    //return res.send({success : "API IS WORKING!"})
+    console.log("this")
+    return res.json({success : {dbb}})
+ })
+
+app.listen(process.env.PORT || 5000)
---
+++ project/src/App.js
@@ -0,0 +1,47 @@
+
+import react from 'react';
+import axios from "axios";
+
+function App() {
+  const [users, setUsers] = react.useState('');
+  
+
+    react.useEffect(() => {
+      axios.get('http://localhost:5000').then((req) =>{
+        console.log(req)
+        setUsers(req.data.success.dbb.users)
+
+      })
+ 
+    }, [])
+
+  return (
+    <div className="App">
+      <h1>Bank Accounts:</h1>
+      {users.map(user =>{
+        return <>
+          <p>ID: {user.id}</p>
+          <p>Cash:{user.cash}</p>
+          <p>Credit:{user.credit}</p>
+        <hr></hr>
+        </>
+      })}
+      {/* <p>ID: {users.id}</p>
+      <p>Cash:{users.cash}</p>
+      <p>Credit:{users.credit}</p> */}
+      
+      {/* {users.map(user =>{
+        return <> 
+        <p>User Id : {user.id}</p>
+        <p>User cash : {user.cash}</p>
+        <p>User credit : {user.credit}</p>
+      
+        
+       </>
+      })} */}
+      
+    </div>
+  );
+}
+
+export default App;
